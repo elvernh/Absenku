532c2569c3f4073a42e0da13a369233ea5182b28
elvernh <87465378+elvernh@users.noreply.github.com>
2025-03-16 15:57:01 +0700

    added sorting changes

--- app/Models/Extracurricular.php
+++ app/Models/Extracurricular.php
@@ -17,10 +17,26 @@ class Extracurricular extends Model
         'school_id'
     ];
 
-    public static function getAll() {
-        $extracurriculars = Extracurricular::all();
+    public static function getAll($sortDirection = 'asc') {
+        $extracurriculars = Extracurricular::all()->sortBy(function ($extracurricular) {
+            // Define the order for the divisions
+            $order = [
+                'SMP' => 1,
+                'SMA' => 2,
+            ];
+    
+            // Return the order value, default to a high number if division is unknown
+            return $order[$extracurricular->division] ?? 999;
+        });
+    
+        // Apply descending order if needed
+        if ($sortDirection === 'desc') {
+            $extracurriculars = $extracurriculars->reverse();
+        }
+    
         return $extracurriculars;
     }
+    
     public function excurVendors():HasMany  
     {
         return $this->hasMany(ExcurVendor::class, 'extracurricular_id');
